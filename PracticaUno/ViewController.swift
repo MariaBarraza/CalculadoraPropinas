876a9b5f7fd8b6e6e2ea689739e3c926fd701c8c
Alumno <ulsa@iMacD105.local>
2019-08-09 13:56:22 -0700

    primer commit

--- PracticaUno/ViewController.swift
+++ PracticaUno/ViewController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTotlaConPropina: UILabel!
+    @IBOutlet weak var lblCantidadPropina: UILabel!
+    @IBOutlet weak var lblPorcentajePropina: UILabel!
+    @IBOutlet weak var sldPorcentajePropina: UISlider!
+    @IBOutlet weak var txtTotalSinPropina: UITextField!
+    
+    
+    
+    @IBAction func doChangeValores(_ sender: Any) {
+        lblPorcentajePropina.text = "\(sldPorcentajePropina.value)"
+        
+        var cantidadPropina = 0.0
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
